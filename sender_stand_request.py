ef77544e481b09c05f63261ca851589d21d97e09
Neat-22 <viloy_evilon@mail.ru>
2024-09-16 20:18:39 +0500

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,20 @@
+import requests
+import configuration
+import data
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)
+#response = post_products_kits(data.product_ids)
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
